820578057fdeba5a5a35e03304fb0b72eb823891
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2022-11-06 18:28:22 +0300

    fix: Dnd for mobile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,22 @@ import type { AppProps } from 'next/app';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
 import { TouchBackend } from 'react-dnd-touch-backend';
-import { useEffect } from 'react';
-
-
-  
+import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  let DnDBackend = HTML5Backend;
+  const [isMobile, setIsMobile] = useState(false);
   useEffect(() =>{
-    const isMobile = /Android|webOS|iPhone|iPad|iPod/i.test(navigator.userAgent);
-    DnDBackend = isMobile ? TouchBackend : HTML5Backend;
+    setIsMobile(/Android|webOS|iPhone|iPad|iPod/i.test(navigator.userAgent));
   }, []);
+
   return (
-    <DndProvider backend={DnDBackend}>
-    <Component {...pageProps} />
-  </DndProvider>
+    <>
+      {!isMobile && <DndProvider backend={HTML5Backend}>
+        <Component {...pageProps} />
+      </DndProvider>}
+      {isMobile && <DndProvider backend={TouchBackend}>
+        <Component {...pageProps} />
+      </DndProvider>}
+    </>
   )
 }
