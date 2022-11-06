aba46fceb335e3556687fa5c5007d95056eba761
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2022-11-06 18:19:00 +0300

    fix: touch devices dnd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,20 @@ import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
+import { TouchBackend } from 'react-dnd-touch-backend';
+import { useEffect } from 'react';
+
+
+  
 
 export default function App({ Component, pageProps }: AppProps) {
+  let DnDBackend = HTML5Backend;
+  useEffect(() =>{
+    const isMobile = /Android|webOS|iPhone|iPad|iPod/i.test(navigator.userAgent);
+    DnDBackend = isMobile ? TouchBackend : HTML5Backend;
+  }, []);
   return (
-  <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={DnDBackend}>
     <Component {...pageProps} />
   </DndProvider>
   )
